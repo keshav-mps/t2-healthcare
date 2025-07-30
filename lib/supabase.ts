14e57a0174afef31593535197ec1ff43f7297a6a
keshav-mps <keshav@pariqsha.com>
2025-07-30 12:26:38 +0530

    chnage path of template-data.json

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import localTemplateData from '../template-data.json';
+import localTemplateData from '../data/template-data.json';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
